eddb1d282c6d5d7c757ebfc739b3d88196c69f14
menaagina <aginamena5@gmail.com>
2023-07-01 13:11:34 -0400

    added required to country textfiled

--- src/components/myAccount/createposts/index.jsx
+++ src/components/myAccount/createposts/index.jsx
@@ -105,6 +105,7 @@ export default function CreatePost() {
             }
           />
           <TextField
+            required
             label="Country"
             fullWidth
             placeholder="Nigeria"
