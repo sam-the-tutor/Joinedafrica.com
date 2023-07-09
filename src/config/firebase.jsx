fa5e9ea0f97fd9ec9817e6e61131ba0a7801e52a
aginamena5 <aginamena5@gmail.com>
2023-07-09 02:45:28 -0400

    added .env file

--- src/config/firebase.jsx
+++ src/config/firebase.jsx
@@ -1,21 +1,18 @@
-
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
-
-export default function startFirebase(){
-    const firebaseConfig = {
-        apiKey: "AIzaSyAFko7loQl4rfXCGuyO_G_OYjq04V0j2jc",
-        authDomain: "joined-africa-notification.firebaseapp.com",
-        databaseURL: "https://joined-africa-notification-default-rtdb.firebaseio.com",
-        projectId: "joined-africa-notification",
-        storageBucket: "joined-africa-notification.appspot.com",
-        messagingSenderId: "69345522595",
-        appId: "1:69345522595:web:1c58a3d41596a6fce9a016",
-        measurementId: "G-TS86FMB5V9"
-      };
-      // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-return getDatabase(app);
-
+export default function startFirebase() {
+  const firebaseConfig = {
+    apiKey: import.meta.env.VITE_APP_APIKEY,
+    authDomain: import.meta.env.VITE_APP_AUTHDOMAIN,
+    databaseURL: import.meta.env.VITE_APP_DATABASEURL,
+    projectId: import.meta.env.VITE_APP_PROJECTID,
+    storageBucket: import.meta.env.VITE_APP_STORAGEBUCKET,
+    messagingSenderId: import.meta.env.VITE_APP_MESSAGINGSENDERID,
+    appId: import.meta.env.VITE_APP_APPID,
+    measurementId: import.meta.env.VITE_APP_MEASUREMENTID,
+  };
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  return getDatabase(app);
 }
