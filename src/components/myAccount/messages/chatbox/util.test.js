e2c3ca68d687be6d3ba8437910970f204b958e2a
aginamena5 <aginamena5@gmail.com>
2023-07-19 15:44:32 -0400

    removed empty test files

--- src/components/myAccount/messages/chatbox/util.test.js
+++
@@ -1,9 +0,0 @@
-import { describe, test, expect } from 'vitest';
-import { sort } from "./util"
-import { unSortedChatMessage, sortedChatMessage } from './mockData';
-
-describe("sort", () => {
-    test("sort messages by date and time in ascending order", () => {
-        expect(sort(unSortedChatMessage)).toEqual(sortedChatMessage);
-    });
-})
